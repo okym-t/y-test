7e8da8876c755d2835cd18a144577b9c3a73146e
okym <cndd97@gmail.com>
2022-05-02 22:25:02 +0900

    chore: emotion導入

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
+  ],
   test: {
     environment: 'happy-dom',
     exclude: [
