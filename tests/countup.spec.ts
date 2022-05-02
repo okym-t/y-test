675c66843af404bd33fb93514f8d9cc876e67ad8
okym <cndd97@gmail.com>
2022-05-02 22:39:16 +0900

    fix: テスト修正

--- tests/countup.spec.ts
+++ tests/countup.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:3001')
+  await page.goto('http://localhost:3000')
 })
 
 test('test', async () => {
