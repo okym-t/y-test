d4170344a7c058635d0611b41615aed0b736b4c7
okym <cndd97@gmail.com>
2022-05-02 20:52:44 +0900

    test: テスト修正

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,11 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'happy-dom',
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/tests/**',
+      '**/.{idea,git,cache,output,temp}/**',
+    ],
   },
 })
