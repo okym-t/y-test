46896a369f745ddf063e14b4474e7001bd627adc
okym <cndd97@gmail.com>
2022-05-03 16:11:32 +0900

    feat: API繋ぎ込み

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { css } from '@emotion/react'
+import { useEffect, useState } from 'react'
 
 const styles = {
   foo: css({
@@ -12,10 +13,20 @@ const styles = {
   }),
 }
 
+const URL = import.meta.env.VITE_API_URL ?? ''
+
 function App() {
+  const [prefectures, setPrefectures] = useState<any | undefined>()
+  useEffect(() => {
+    fetch(`${URL}/api/v1/prefectures`)
+      .then((response) => response.json())
+      .then((json) => setPrefectures(json))
+  }, [])
+
+  if (!prefectures) return <p>loading...</p>
   return (
     <div css={styles.foo}>
-      <p css={styles.bar}>test</p>
+      <p css={styles.bar}>{JSON.stringify(prefectures)}</p>
     </div>
   )
 }
